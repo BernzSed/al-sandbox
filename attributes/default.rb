a6a281f7dd7f98272b6f3db3260f26a0878d9669
Bernie Stern <bernzs@gmail.com>
2016-05-10 12:40:54 -0400

    file added

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+default["geosort"]["tunable"]["esrewards_tag"] = "latest"
+default["geosort"]["tunable"]["rewards_search_updater_tag"] = "331"
+default["geosort"]["tunable"]["rewards_search_service_tag"] = "783"
+default["geosort"]["tunable"]["logspout_tag"] = "20150102"
+
+case node["env_type"]
+when "dev"
+  default["geosort"]["tunable"]["num_kafkas"] = 1
+else
+  default["geosort"]["tunable"]["num_kafkas"] = 3
+end
